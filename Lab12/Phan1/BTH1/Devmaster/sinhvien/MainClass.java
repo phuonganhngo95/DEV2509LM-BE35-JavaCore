4993ef5034189bc63c7473100362dc1c144f4c0d
Ngo Phuong Anh <142812823+phuonganhngo95@users.noreply.github.com>
2025-12-10 21:47:24 +0700

    Delete Lab12 directory

--- Lab12/Phan1/BTH1/Devmaster/sinhvien/MainClass.java
+++
@@ -1,9 +0,0 @@
-package Lab12.Phan1.BTH1.Devmaster.sinhvien;
-
-public class MainClass {
-    public static void main(String[] args) {
-        db demo = new db();
-        demo.createFolder("Devmaster/slide");
-        demo.createFile("Devmaster/slide/sinhvien.db");
-    }
-}
